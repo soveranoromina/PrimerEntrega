64b6671adb0fe871562e7c3ce0c3d758b3662996
rominasoveranocode <rominasoverano.trabajo@gmail.com>
2025-08-05 17:27:55 -0300

    CartManager

--- src/manager/CartManager.js
+++ src/manager/CartManager.js
@@ -1,5 +1,6 @@
 import fs from "fs";
-import { idManager } from "./validations/IdManager.js";
+import { validator } from "./validations/Validator.js"
+import { productManager } from "../manager/ProductManager.js"
 
 class CartManager {
     constructor(path) {
@@ -10,7 +11,6 @@ class CartManager {
         try {
             if (fs.existsSync(this.path)) {
                 const carts = await fs.promises.readFile(this.path, "utf-8");
-                console.log(carts)
                 return JSON.parse(carts);
             }
         } catch (error) {
@@ -18,44 +18,43 @@ class CartManager {
         }
     }
 
-    getProductsFromCart = async (id) => {
+    getCartById = async (id) => {
         try {
             const carts = await this.getCarts();
             const cart = carts.find((c) => c.id === Number(id));
-            if (!cart) throw new Error("Producto no encontrado");
+
+            if (!cart) throw new Error("Carrito no encontrado");
+            return cart;
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    getProductsFromCart = async (id) => {
+        try {
+            const cart = await this.getCartById(id)
             return cart.products;
         } catch (error) {
             throw error;
         }
     }
 
-    createCart = async (obj) => {
+    createCart = async () => {
 
         try {
-            if (!obj) throw new Error("Debes enviar por lo menos un valor")
-            const { products } = obj
-            const cart_values = { products }
-
             const carts = await this.getCarts();
-            console.log(carts)
-            const id = idManager.generate(carts);
-
-            for (const [key, value] of Object.entries(cart_values)) {
-                if (!value) {
-                    return `Falta el campo ${key}`
-                }
-            }
+            const id = validator.generateId(carts);
 
             const cart = {
                 id: id,
-                products
+                products: []
             }
-
             carts.push(cart)
-
             await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 2))
             return {
-                message: `El carrito de ID ${cart.id} ha sido creado con éxito`
+                cart,
+                status: "new" 
             }
         } catch (error) {
             throw error
@@ -65,26 +64,27 @@ class CartManager {
 addProductToCart = async (idCart, idProduct) => {
     try {
         const carts = await this.getCarts();
+        await this.getCartById(idCart)
+        await productManager.getProductById(idProduct);
+
         const cartIndex = carts.findIndex(c => c.id === Number(idCart));
-
-        if (cartIndex === -1) throw new Error("Carrito no encontrado");
-
         const cart = carts[cartIndex];
+        const productInCart = cart.products.find(p => p.id === Number(idProduct));
 
-        const product = cart.products.find(p => p.id === Number(idProduct));
-
-        if (product) {
-             product.quantity += 1;
-        } else {
+        if (!productInCart) {
             cart.products.push({
                 id: Number(idProduct),
                 quantity: 1
             });
+        } else {
+            productInCart.quantity += 1;
         }
+
         await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 2));
 
         return {
-            message: `Producto ${idProduct} agregado al carrito ${idCart}`
+                cart,
+                status: "added"
         };
 
     } catch (error) {
